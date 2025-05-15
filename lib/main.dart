88fa31555e6fc920e503c4cc8667bd06aedbbeea
FAT-FATMA <fmohsen025@gmail.com>
2025-05-15 20:13:51 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,72 +7,19 @@ import 'package:cure/screens/forget_password.dart';
 import 'package:cure/screens/signup_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-/*void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
-  runApp(MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      checkLoginStatus();
-    });
-  }
-
-  Future<void> checkLoginStatus() async {
-    final prefs = await SharedPreferences.getInstance();
-    final bool isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
-    await Future.delayed(Duration(milliseconds: 100)); // نأجل شوية
-
-    if (isLoggedIn) {
-      Navigator.pushReplacementNamed(context, '/home');
-    } else {
-      Navigator.of(context, rootNavigator: true).pushReplacementNamed('/login');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: WelcomeScreen(),
-      routes: {
-        '/signup': (context) => SignUpScreen(),
-        '/login': (context) => LoginScreen(),
-        '/forgot-password': (context) => ForgetpassScreen(),
-        //'/home': (context) => HomeScreen(),
-      },
-    );
-  }
-}*/
-
-// استيردي باقي الملفات اللي عندك (شاشات، firebase options)
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   final prefs = await SharedPreferences.getInstance();
-  final bool isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
 
-  runApp(MyApp(isLoggedIn: isLoggedIn));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final bool isLoggedIn;
 
-  const MyApp({super.key, required this.isLoggedIn});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -83,7 +30,6 @@ class MyApp extends StatelessWidget {
         '/signup': (context) => SignUpScreen(),
         '/login': (context) => LoginScreen(),
         '/forgot-password': (context) => ForgetpassScreen(),
-        // لما تضيفي شاشة Home حطيها هنا كمان
       },
     );
   }
